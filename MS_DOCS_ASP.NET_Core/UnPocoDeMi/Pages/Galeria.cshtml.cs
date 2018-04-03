d7f125f0dcb0d03f5e4913c0de357cda85fdeb29
Joel Jesus <joeljesusmot@gmail.com>
2018-04-02 20:06:47 -0400

    proyectos adicionales

---
+++ MS_DOCS_ASP.NET_Core/UnPocoDeMi/Pages/Galeria.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnPocoDeMi.Pages
+{
+    public class GaleriaModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Imagenes de los momentos y logros de Joel en la programacion.";
+        }
+    }
+}
